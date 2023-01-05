d1dec0e9db9a19ab1e38aaac10fcf1a7b788afae
Sannidhya <sannidhyamaheshwari1328@gmail.com>
2023-01-05 21:42:15 +0530

    3rd commit

--- 1.py
+++ 1.py
@@ -1,3 +1,3 @@
 n1 = int(input("enter first number: "))
 n2 = int(input("enter second number: "))
-print("Sum is",n1+ n2,"and Product is",n1*n2)
+print("Sum is",n1+ n2,"and Product is",n1*n2,"and division is",n1/n2)
